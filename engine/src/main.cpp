ad60261d4e0af50baa1df376e2ce0607f416e7e4
Adrian Frąckowiak <67045837+adrianfrackowiak@users.noreply.github.com>
2026-07-09 19:12:15 +0200

    feature(engine): #3 cpp engine init (#7)
    * feature(engine): cpp init
    
    * fixes
    
    * f

---
+++ engine/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "gochessplusplus.dev v0.0.1 init\n";
+    return 0;
+}
---
+++ engine/tests/test.cpp
@@ -0,0 +1,3 @@
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("build pipeline sanity check", "[placeholder]") { REQUIRE(1 + 1 == 2); }
